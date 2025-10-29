c6fdb829b893085050f4f7c799198b17418762bb
koundia <hibrahimakoundia@gmail.com>
2025-10-29 19:36:10 +0100

    mise en place de la list de tache et des actions sur cette liste

--- src/components/TaskContainer.jsx
+++ src/components/TaskContainer.jsx
@@ -2,12 +2,32 @@ import {Header} from "./header/Header.jsx";
 import {TaskInput} from "./taskInput/TaskInput.jsx";
 import {TaskList} from "./taskList/TaskList.jsx";
 import {Footer} from "./footer/Footer.jsx";
+import {useState} from "react";
 
 export const TaskContainer = () => {
+	const [tasks, setTasks] = useState([{
+		id: 1,
+		title: "Task 1",
+		isDone: false
+	}]);
+	const actions = {
+		addTask:  (title) => {
+			tasks.push({id: tasks.length+1, title: title, isDone: false});
+		},
+		deleteTask:  (id) => {
+			tasks.splice(id, 1);
+		},
+		changeStatus:  (id,status) => {
+			tasks[id].status = status;
+		},
+		getAllTasks: () => {
+			return tasks;
+		}
+	}
 	return (
 		<main>
 			<Header />
-			<TaskInput />
+			<TaskInput addTask={actions.addTask} />
 			<TaskList />
 			<Footer />
 		</main>
--- src/components/taskInput/TaskInput.jsx
+++ src/components/taskInput/TaskInput.jsx
@@ -1,12 +1,17 @@
 import styles from './TaskInput.module.css';
-export const TaskInput = () => {
-	const placeHolder = "indiquer une titre de tache explicite"
+import {useState} from "react";
+export const TaskInput = ({addTask}) => {
+	const placeHolder = "indiquer une titre de tache explicite";
+	const [taskTitle, setTaskTitle] = useState("");
+	const handleInputChange = (e) => {
+		setTaskTitle(e.target.value);
+	}
 	return (
 		<div className={styles.container}>
 			<h2>Ajouter ta prochaine tache</h2>
 			<form>
-				<input type="text" className={styles.inputField} placeholder={placeHolder}/>
-				<button className={styles.addBtn} type="submit">Ajouter</button>
+				<input type="text" onChange={handleInputChange} className={styles.inputField} placeholder={placeHolder}/>
+				<button className={styles.addBtn} type="submit" >Ajouter</button>
 			</form>
 
 		</div>
