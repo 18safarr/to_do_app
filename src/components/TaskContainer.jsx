724dfae435cafc064097c95c9835968e6c1d812e
koundia <hibrahimakoundia@gmail.com>
2025-10-29 10:57:12 +0100

    mise en place du composant header

---
+++ src/components/TaskContainer.jsx
@@ -0,0 +1,9 @@
+import {Header} from "./header/Header.jsx";
+
+export const TaskContainer = () => {
+	return (
+		<main>
+			<Header />
+		</main>
+	)
+}
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import styles from './Header.module.css'
+import logoReact from '../../assets/react.svg'
+export const Header = () => {
+	return (
+		<div className={styles.container}>
+			<div className={styles.titleContainer}>
+				<img src={logoReact} alt="logo" width={50} height={50} />
+				<div>
+					<h1>ToDoApp</h1>
+					<div>
+						<code>Organiser votre vie</code>
+					</div>
+				</div>
+
+			</div>
+			<code className="color-primary">v1.0</code>
+		</div>
+	)
+}
