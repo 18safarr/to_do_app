2cc1f465085a83a4fe98b83594544fbd12a763af
koundia <hibrahimakoundia@gmail.com>
2025-10-30 12:43:12 +0100

    persistance des donnees dans le local storage

--- src/components/TaskContainer.jsx
+++ src/components/TaskContainer.jsx
@@ -2,26 +2,41 @@ import {Header} from "./header/Header.jsx";
 import {TaskInput} from "./taskInput/TaskInput.jsx";
 import {TaskList} from "./taskList/TaskList.jsx";
 import {Footer} from "./footer/Footer.jsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export const TaskContainer = () => {
 	const [tasks, setTasks] = useState([]);
+	useEffect(() => {
+		const storedTasks = localStorage.getItem("tasks");
+		if (storedTasks) {
+			setTasks(JSON.parse(storedTasks));
+		}
 
+
+
+	}, []);
+	const saveTaskOnLocalStorage = (tasks) => {
+		localStorage.setItem("tasks", JSON.stringify(tasks));
+	}
 	const actions = {
 		addTask:  (title) => {
 			const idTast = tasks.length ? tasks[tasks.length-1].id + 1 : 1;
 			const newTask = {id: idTast, title: title, isDone: false}
-			setTasks([...tasks, newTask]);
+			const newsTasks = [...tasks, newTask]
+			setTasks(newsTasks);
+			saveTaskOnLocalStorage(newsTasks);
 		},
 		deleteTask:  (id) => {
 			const newsTasks = tasks.filter(task => task.id !== id);
 			setTasks(newsTasks);
+			saveTaskOnLocalStorage(newsTasks);
 		},
 		changeStatus:  (id,status) => {
 			const newTasks = tasks.map((task) =>
 				task.id === id ? { ...task, isDone: status } : task
 			);
 			setTasks(newTasks);
+			saveTaskOnLocalStorage(newTasks);
 		},
 		getCountTasks : ()=>{
 			const countTasksDone = tasks.filter(task => task.isDone).length;
